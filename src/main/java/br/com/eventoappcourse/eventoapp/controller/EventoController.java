4db71a601ba981c18c26524761039899620a7058
Mateus Alabarse <mateusalabarse@hotmail.com>
2022-01-22 13:25:47 -0300

    css materialize applie in project and listAll feature implemented

--- src/main/java/br/com/eventoappcourse/eventoapp/controller/EventoController.java
+++ src/main/java/br/com/eventoappcourse/eventoapp/controller/EventoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EventoController {
@@ -24,5 +25,13 @@ public class EventoController {
         
         eventoRepository.save(evento);
         return "redirect:/cadastrarEvento";
+    }
+    
+    @GetMapping("/eventos")
+    public ModelAndView listAll() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        Iterable<Evento> eventos = eventoRepository.findAll();
+        modelAndView.addObject("eventos", eventos);
+        return modelAndView;
     }
 }
--- src/main/java/br/com/eventoappcourse/eventoapp/repository/EventoRepository.java
+++ src/main/java/br/com/eventoappcourse/eventoapp/repository/EventoRepository.java
@@ -2,7 +2,9 @@ package br.com.eventoappcourse.eventoapp.repository;
 
 import br.com.eventoappcourse.eventoapp.models.Evento;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventoRepository extends CrudRepository<Evento, Long> {
     
 }
