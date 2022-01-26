b4b46eb116db5d04ae1a1cb89024f382591ef125
Mateus Alabarse <mateusalabarse@hotmail.com>
2022-01-25 21:23:20 -0300

    EventsDetail method implemented

--- src/main/java/br/com/eventoappcourse/eventoapp/controller/EventoController.java
+++ src/main/java/br/com/eventoappcourse/eventoapp/controller/EventoController.java
@@ -34,4 +34,8 @@ public class EventoController {
         modelAndView.addObject("eventos", eventos);
         return modelAndView;
     }
+    
+    public String eventoDetails() {
+        
+    }
 }
