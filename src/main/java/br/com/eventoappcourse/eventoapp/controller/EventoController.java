01536c72afd752446ae20ef6cc2d6b91d04dbbd9
Mateus Alabarse <mateusalabarse@hotmail.com>
2022-02-01 17:52:43 -0300

    Delete guest method implemented

--- src/main/java/br/com/eventoappcourse/eventoapp/controller/EventoController.java
+++ src/main/java/br/com/eventoappcourse/eventoapp/controller/EventoController.java
@@ -6,6 +6,7 @@ import br.com.eventoappcourse.eventoapp.models.Evento;
 import br.com.eventoappcourse.eventoapp.repository.ConvidadosRepository;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
